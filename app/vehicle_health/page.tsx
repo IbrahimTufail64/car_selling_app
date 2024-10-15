81ccba9993f8ca5bb8a7855aafe833d2147b7900
Ibrahim Tufail <ibrahimsomeonenotimportant@gmail.com>
2024-10-15 18:46:35 +0500

    final testing car health

--- app/vehicle_health/page.tsx
+++ app/vehicle_health/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 import Link from 'next/link'
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { IoChevronBack } from 'react-icons/io5'
 import splash from '@/assets/icons/Rays-small.png'
 import front from '@/assets/Front_Damage-modified-modified.png'
@@ -8,16 +8,73 @@ import back from '@/assets/Back_Damage-modified-modified.png'
 import roof from '@/assets/Roof_Damage-modified-modified.png'
 import driverSide from '@/assets/Driver_Damage-modified-modified.png'
 import PassengerSide from '@/assets/Passenger_Damage-modified-modified.png'
+import mark from '@/assets/Damage_mark.png'
 
 const VehicleHealth = () => {
 
     const [currentSide, setCurrentSide] = useState('Driver Side');
-    const [damage, setDamage] = useState([
-                                    [0,0],[0,0],[0,0],[0,0],[0,0] ])
+    const [size, setSize] = useState('Small');
+
+    const [damage, setDamage] = useState({
+                                    'Driver Side' : {
+                                        'size': 'Small',
+                                        'coordinates': { x: 0, y: 0 }
+                                    },
+                                    'Passenger side': {
+                                        'size': 'Small',
+                                        'coordinates': { x: 0, y: 0 }
+                                    },
+                                    'Front side': {
+                                        'size': 'Small',
+                                        'coordinates': { x: 0, y: 0 }
+                                    },
+                                    'Back side': {
+                                        'size': 'Small',
+                                        'coordinates': { x: 0, y: 0 }
+                                    },
+                                    'Roof side':{
+                                        'size': 'Small',
+                                        'coordinates': { x: 0, y: 0 }
+                                    },
+                                })
 
                                   
 
-    const [coordinates, setCoordinates] = useState([0,0]);
+                                    const canvasRef = useRef(null);
+                                    const [coordinates, setCoordinates] = useState({ x: 0, y: 0 });
+                                  
+                                    const handleClick = (event:any) => {
+                                      const canvas: any = canvasRef.current;
+                                      if (!canvas) return;
+                                  
+                                      // Get canvas position relative to the viewport
+                                      const canvasRect = canvas.getBoundingClientRect();
+                                      const relativeX = canvasRect.left;
+                                      const relativeY = canvasRect.top;
+                                  
+                                      // Calculate mouse position relative to canvas
+                                      const clientX = event.clientX;
+                                      const clientY = event.clientY;
+                                      const x = Math.abs(clientX - relativeX);
+                                      const y = Math.abs(clientY - relativeY);
+                                  
+                                      setCoordinates({ x, y });
+                                      let temp_damage:any = damage;
+                                    temp_damage[currentSide].coordinates = {x,y};
+                                    setDamage(temp_damage);
+                                  
+                                  
+                                      console.log(`Clicked at: X - ${x}, Y - ${y}`);
+                                    };
+                                  
+                                    useEffect(() => {
+                                      const canvas: any = canvasRef.current;
+                                      if (canvas) {
+                                        canvas.addEventListener('click', handleClick); // Use 'click' event
+                                  
+                                        return () => canvas.removeEventListener('click', handleClick);
+                                      }
+                                    }, [canvasRef]); // Dependency on canvasRef
 
 
     const sides:any = {
@@ -28,6 +85,12 @@ const VehicleHealth = () => {
                         'Roof side':roof,
     }
 
+    const sizes:any ={
+        'Small' : 40,
+        'Medium' : 50,
+        'Large': 70
+    }
+
     const sidesArray = [
         'Driver Side',
         'Passenger side',
@@ -36,34 +99,38 @@ const VehicleHealth = () => {
         'Roof side'
     ]
 
-    // useEffect(()=>{
-    //     console.log(damage);
-    // },[...damage])
 
-    const handle_damage_marks = (e: any)=>{
-        let coordinates =  [e.screenX,e.screenY];
-        setCoordinates(coordinates);
-        sidesArray.map((e,i)=>{
-            if(e === currentSide){
-                let temp_damage = damage;
-                temp_damage[i] = coordinates;
-                setDamage(temp_damage); 
-                console.log(temp_damage)
-            }
-        })
+    const handleSize = (e:any) =>{
+        setSize(e.target.value);
+        let temp_damage:any = damage;
+        temp_damage[currentSide].size = e.target.value;
+        temp_damage[currentSide].coordinates = coordinates;
+        setDamage(temp_damage);
+        console.log(temp_damage)
     }
 
-    const top = `${Math.abs(coordinates[1])}px`
-    const left = `${Math.abs(coordinates[0])}px`
+    const handleSideChange = (e:any)=>{
+        setCurrentSide(e);
+        let temp_damage:any = damage;
+        const x = temp_damage[currentSide].coordinates.x;
+        const y = temp_damage[currentSide].coordinates.y;
+        console.log(x,y,temp_damage[currentSide].size);
+        setCoordinates({x,y});
+        setSize(temp_damage[currentSide].size);
+    }
 
   return (
-    <div onClick={e => {handle_damage_marks(e)}}>
+    <div >
          <div className='bg-secondary w-full h-[120vh] overflow-hidden relative' >
+         <canvas ref={canvasRef} width="800" height="600" className="absolute"  onClick={handleClick} />
          
-         <img src={sides[currentSide].src} className='w-[649px] h-[464px] absolute top-0 left-[200px]' />
 
-         <div className={`w-5 h-5 bg-fourth absolute `}
-         style={{top: top, left: left}}></div>
+         <div
+            className="bg-secondary absolute rounded-full ${mt-[]}" // Style indicator
+            style={{ top: coordinates.y-Math.floor(sizes[size]/2), left: coordinates.x-Math.floor(sizes[size]/2) }}
+        >
+            <img src={mark.src} className={`w-15 h-15 `} style={{width: sizes[size],height: sizes[size]}}/>
+        </div>
 
         <div className='p-5 py-3 flex space-x-2 text-[18px] justify-between'>
             <div className='flex space-x-2 pt-2'>
@@ -84,15 +151,15 @@ const VehicleHealth = () => {
         <div className='flex'>
             <div className='space-y-2 px-3 mt-3'> 
             <div className='bg-white p-4 px-5 rounded-md border border-1 text-[18px] flex relative space-x-4 border border-1 border-[#D3D4FD]'>
-                <input type="radio" id="Small" name="colors" value="Small" className='absolute left-[-40px] top-[25px]' />
+                <input type="radio" id="Small" name="colors" value="Small" className='absolute left-[-40px] top-[25px]' onChange={(e)=>{handleSize(e)}}/>
                 <label htmlFor="Small" className='space-y-[-3px] pl-5'>
                     <div >Small</div>
-                    <div className='text-slate-500 text-[13px]'>6-15cm</div>
+                    <div className='text-slate-500 text-[13px]'>0-5cm</div>
                 </label>
                 </div>
 
                 <div className='bg-white p-4 px-5 rounded-md border border-1 text-[18px] flex relative space-x-4 border border-1 border-[#D3D4FD]'>
-                <input type="radio" id="Medium" name="colors" value="Medium" className='absolute left-[-40px] top-[25px]' />
+                <input type="radio" id="Medium" name="colors" value="Medium" className='absolute left-[-40px] top-[25px]' onChange={(e)=>{handleSize(e)}}/>
                 <label htmlFor="Medium" className='space-y-[-3px] pl-5'>
                     <div >Medium</div>
                     <div className='text-slate-500 text-[13px]'>6-15cm</div>
@@ -100,17 +167,17 @@ const VehicleHealth = () => {
                 </div>
 
                 <div className='bg-white p-4 px-5 rounded-md border border-1 text-[18px] flex relative space-x-4 border border-1 border-[#D3D4FD]'>
-                <input type="radio" id="Large" name="colors" value="Large" className='absolute left-[-40px] top-[25px]' />
+                <input type="radio" id="Large" name="colors" value="Large" className='absolute left-[-40px] top-[25px]' onChange={(e)=>{handleSize(e)}}/>
                 <label htmlFor="Large" className='space-y-[-3px] pl-5'>
                     <div >Large</div>
-                    <div className='text-slate-500 text-[13px]'>6-15cm</div>
+                    <div className='text-slate-500 text-[13px]'>16cm+</div>
                 </label>
                 </div>
                 
                 
             </div>
-            <div className='mt-[-80px] w-[649px] h-[464px]' >
-                {/* <img src={sides[currentSide].src} className='w-[649px] h-[464px] '/> */}
+            <div className='mt-[-60px] w-[649px] h-[464px]' >
+                <img src={sides[currentSide].src} className='w-[584px] h-[417px] '/>
             </div>
         </div>
 
@@ -120,7 +187,7 @@ const VehicleHealth = () => {
                 {
                     sidesArray.map((e,i)=>{
                         return <div className={`border border-1 border-[#D3D4FD] px-7 p-4 ${i === 0 && 'rounded-l-full'} ${i === 4 && 'rounded-r-full'} ${e === currentSide ? 'bg-fourth text-white' : 'bg-white'}`}
-                                onClick={()=>{setCurrentSide(e)}} >{e}</div>
+                                onClick={()=>{handleSideChange(e)}} >{e}</div>
                     })
                 }
             </div>
